e70cd3cc03682e25785d399f4c026f6edf294f4e
William Campany <william.campany@gmail.com>
2021-02-06 16:46:22 +0000

    it no work dom, why?

--- Application/Backend/api/src/backend_apiconfig.py
+++ Application/Backend/api/src/backend_apiconfig.py
@@ -1,6 +1,7 @@
+import os
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "postgresql://safeuser:password@db/api_db"
-
+SQLALCHEMY_DATABASE_URI = str(os.getenv('DB_URI'))
+print('my name is ' + os.getenv('DB_URI'))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DATABASE_CONNECT_OPTIONS = {}
 THREADS_PER_PAGE = 2
