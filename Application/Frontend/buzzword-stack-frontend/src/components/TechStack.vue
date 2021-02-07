96403a50c0ece97659f9058eb0cae06aa7c1178d
Dom Hall <dominiczy.hall@gmail.com>
2021-02-07 14:56:04 +0000

    Added new content to frontend

---
+++ Application/Frontend/buzzword-stack-frontend/src/components/TechStack.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1><Count/></h1>
+    <h1>Buzzword Tech Stack</h1>
+    <ul>
+        <li>Cloud Deployment</li>
+        <li>Flask</li>
+        <li>Vue</li>
+        <li>Docker</li>
+        <li>NGINX</li>
+        <li>Trello</li>
+        <li>Axios</li>
+        <li>kubernetes</li>
+        <li>Postgresql</li>
+        <li>SQLalchemy</li>
+        <li>DigitalOcean</li>
+        <li>Docker-compose</li>
+        <li>Terraform</li>
+        <li>Flutter</li>
+        <li>pytest</li>
+        <li>jest</li>
+    </ul>
+    <h2>Pushing Images to Registry and Kubernetes</h2>
+    <img alt="Pushing Images Diagram" src="@/assets/ImagePushProcess.svg">
+    <h2>Users accessing our website</h2>
+    <img alt="Pushing Images Diagram" src="@/assets/DOInf.svg">
+  </div>
+</template>
+
+<script>
+import Count from '@/components/Count.vue'
+export default {
+  name: 'TechStack',
+  props: {
+  },
+  components: {
+    Count
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ Application/Frontend/buzzword-stack-frontend/src/components/Welcome.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1><Count/></h1>
+    <h1>Buzzword Tech Stack</h1>
+    <p>The greatest counting application you will ever see</p>
+    <h2>The Goals</h2>
+    <ul>
+        <li>Cloud Deployment</li>
+        <li>Infrastructure as Code</li>
+        <li>Deployment Pipeline on PR merge</li>
+        <li>Scaling microservices</li>
+        <li>DNS managed by code</li>
+        <li>Template for future projects</li>
+        <li>Infrastructure Deployment Pipeline on Cloud</li>
+    </ul>
+    <h2>What we achieved</h2>
+    <ul>
+        <li>Cloud Deployment</li>
+        <li>Infrastructure as Code</li>
+        <li>Deployment Pipeline on PR merge</li>
+        <li>Scaling microservices</li>
+        <li>DNS managed by code</li>
+        <li>Template for future projects</li>
+    </ul>
+    <h2>What we didn't achieve</h2>
+    <ul>
+        <li>Infrastructure Deployment Pipeline on Cloud</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Count from '@/components/Count.vue'
+export default {
+  name: 'Welcome',
+  props: {
+  },
+  components: {
+    Count
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
