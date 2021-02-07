cffda555c0dea8d57ab60435867bc875bed0966d
kaloianbch <git@chokanov.com>
2021-02-07 13:24:53 +0000

    fail: test

--- Application/Frontend/buzzword-stack-frontend/tests/unit/example.spec.js
+++ Application/Frontend/buzzword-stack-frontend/tests/unit/example.spec.js
@@ -1,12 +1,4 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+it('runs a test :)))))))))', () => {
+  const num = !true
+  expect("false").toMatch(num.toString());
 });
