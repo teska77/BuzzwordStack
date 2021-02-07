9145fb32c398127b7f85b90d87dbc56054cfa32c
Dom Hall <dominiczy.hall@gmail.com>
2021-02-07 20:37:27 +0000

    allow-autoscaling-without-default-nodes

--- Infrastructure/cluster.tf
+++ Infrastructure/cluster.tf
@@ -20,7 +20,6 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
     name       = "buzzword-stack-cluster-pool"
     size       = "s-1vcpu-2gb"
     auto_scale = true
-    node_count = 1
     min_nodes  = 1
     max_nodes  = 5
   }
